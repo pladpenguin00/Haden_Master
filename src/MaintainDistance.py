bdbf1e3199186b4515de7489361c8cb11a2716a1
pladpenguin00 <smithhhaden@gmail.com>
2018-03-06 16:57:44 -0500

    simplified

--- src/MaintainDistance.py
+++ src/MaintainDistance.py
@@ -13,23 +13,6 @@ class MaintainDistanceNode(object):
 	    
 #Initial
 	#collect 100 bbox heights 
-
-		count=0
-		while count < 100
-			rospy.Subscriber("bbox", bbox, self.bbox_callback)
-		
-			height[count] = bbox_bottom - bbox_top
-			np.insert(a, count, height)
-			count=count+1
-		#find distribution
-
-		
-		#find standard deviation 
-		std_dev100 = stdev(a)
-		mu100=mean(a)
-		plus100=mu100+std_dev100
-		minus100=mu100-std_dev100
-		
 		rospy.Subscriber("bbox", bbox, self.bbox_callback)
 		rospy.spin()
 	
@@ -40,28 +23,49 @@ class MaintainDistanceNode(object):
 		global height
 		height = data
 		
-		main(code)
+		if switch == 0
+			count=0
+			while count < 100
+		
+				height[count] = bbox_bottom - bbox_top
+				np.insert(a, count, height)
+				count=count+1
+			#find distribution
+
+		
+			#find standard deviation 
+			std_dev100 = stdev(a)
+			mu100=mean(a)
+			plus100=mu100+std_dev100
+			minus100=mu100-std_dev100
+			if count == 99
+				switch = 1
+				
+		
+		if switch == 1
+			while count < 10
+	
+				height = bbox_bottom - bbox_top
+				np.insert(b, count, height)
+				count=count+1
+				#find distribution
+
+				#find standard deviation
+			std_dev10 = stdev(a)
+			mu10=mean(a)
+			plus10=mu10+std_dev10
+			minus10=mu10+std_dev10
+			maincode(minus10, plus10, minus100, plus100)
+
 		
  def check_height()
 	#collect 10 bbox heights
 	count=0	
 	b=0
-	while count < 10
-		rospy.Subscriber("bbox", bbox, self.bbox_callback)
-	
-		height = bbox_bottom - bbox_top
-		np.insert(b, count, height)
-		count=count+1
-		#find distribution
 
-		#find standard deviation
-		std_dev10 = stdev(a)
-		mu10=mean(a)
-		plus10=mu10+std_dev10
-		minus10=mu10+std_dev10
 
 		
- def maincode()	 #if the height gets bigger back up till back at height
+ def maincode(minus10, plus10, minus100, plus100)	 #if the height gets bigger back up till back at height
 	check_height()
 	while plus10 > plus100
 		motor=-0.1
@@ -81,7 +85,7 @@ if __name__ == '__main__':
     try:
         # This will initialize our class, calling ThesisMasterNode.__init__ implicitly
         tmn = MaintainDistanceNode()
-
+		
 
 	except rospy.ROSInterruptException:
 	   pass
